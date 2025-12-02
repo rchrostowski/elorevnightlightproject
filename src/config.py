386bd73b588aa134d41a5bd5869a04101d5121fe
rchrostowski <144856089+rchrostowski@users.noreply.github.com>
2025-12-02 09:12:53 -0500

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,1 +1,19 @@
+# src/config.py
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+
+DATA_DIR = PROJECT_ROOT / "data"
+DATA_RAW = DATA_DIR / "raw"
+DATA_INTER = DATA_DIR / "intermediate"
+DATA_FINAL = DATA_DIR / "final"
+
+DATA_INTER.mkdir(parents=True, exist_ok=True)
+DATA_FINAL.mkdir(parents=True, exist_ok=True)
+
+# Filenames
+LIGHTS_RAW_FILE = "VIIRS-nighttime-lights-2013m1to2024m5-level2.csv"
+SP500_RAW_FILE = "sp500_clean.csv"
+RETURNS_RAW_FILE = "sp500_monthly_returns.csv"  # adjust if your file is named differently
+MODEL_DATA_FILE = "nightlights_model_data.csv"
 
